ee8da8dfb4ad18e38523ac8c1f9f7d141f18c6eb
tomas <Tomas>
2024-01-22 03:20:39 -0500

    Remove unuse css, deploy and bugfix on stars component

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAGd4HjAORQ2cf-bYvdAVi-u83lP1Vv_dM",
+  authDomain: "dev-bookflix.firebaseapp.com",
+  databaseURL: "https://dev-bookflix.firebaseio.com",
+  projectId: "dev-bookflix",
+  storageBucket: "dev-bookflix.appspot.com",
+  messagingSenderId: "355497591795",
+  appId: "1:355497591795:web:ab130f8089d821e5"
+};
+
